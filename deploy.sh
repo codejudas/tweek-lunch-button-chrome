9557c21551684a63a492839c6e09734daa56dd84
Evan Fossier <efossier@twilio.com>
2017-01-27 12:42:32 -0800

    Add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-mv src.crx tweek-lunch-button.crx
-scp ./tweek-lunch-button.crx evan@lunch-button.online:tweek-lunch-button.crx
+mv src.crx tweek-lunch-button.crx || true
+scp ./tweek-lunch-button.crx evan@lunch-button.online:/tweek/tweek-lunch-button.crx
